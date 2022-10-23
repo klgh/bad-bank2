172ea1609dee9265467b8e0c1dd5e4edc250ec32
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2022-10-22 20:23:33 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { UserContext } from './context';
+import Navbar from './navbar';
+import Home from './home';
+import CreateAccount from './createaccount';
+import Deposit from './deposit';
+import Withdraw from './withdraw';
+import AllData from './alldata';
+import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+      <Navbar />
+      <UserContext.Provider value={{users:[{name:'Max',email:'max@mit.edu',password:'secret',balance:1000}]}}>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create-account' element={<CreateAccount />} />
+          <Route path='/deposit' element={<Deposit />} />
+          <Route path='/withdraw' element={<Withdraw />} />
+          <Route path='/all-data' element={<AllData />} />
+        </Routes>
+      </UserContext.Provider>
+   </BrowserRouter>
   );
 }
 
