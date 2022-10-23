569ae1e235cee4c75f4bba30f81175fb2019d3da
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2022-10-23 16:15:12 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import AllData from "./alldata";
 import "./app.css";
 import { UserContext } from "./context";
@@ -10,29 +10,31 @@ import Withdraw from "./withdraw";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <UserContext.Provider
-        value={{
-          users: [
-            {
-              name: "Max",
-              email: "max@mit.edu",
-              password: "secret",
-              balance: 1000,
-            },
-          ],
-        }}
-      >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-account" element={<CreateAccount />} />
-          <Route path="/deposit" element={<Deposit />} />
-          <Route path="/withdraw" element={<Withdraw />} />
-          <Route path="/all-data" element={<AllData />} />
-        </Routes>
-      </UserContext.Provider>
-    </BrowserRouter>
+    <>
+      <HashRouter>
+        <Navbar />
+        <UserContext.Provider
+          value={{
+            users: [
+              {
+                name: "Max",
+                email: "max@mit.edu",
+                password: "secret",
+                balance: 1000,
+              },
+            ],
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-account" element={<CreateAccount />} />
+            <Route path="/deposit" element={<Deposit />} />
+            <Route path="/withdraw" element={<Withdraw />} />
+            <Route path="/all-data" element={<AllData />} />
+          </Routes>
+        </UserContext.Provider>
+      </HashRouter>
+    </>
   );
 }
 
