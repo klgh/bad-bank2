59a9aaa7e679c73885aec77ac53124e13eb34eae
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2022-10-23 16:56:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import { UserContext } from './context';
-import Navbar from './navbar';
-import Home from './home';
-import CreateAccount from './createaccount';
-import Deposit from './deposit';
-import Withdraw from './withdraw';
-import AllData from './alldata';
-import './app.css';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { UserContext } from "./context";
+import Navbar from "./navbar";
+import Home from "./home";
+import CreateAccount from "./createaccount";
+import Deposit from "./deposit";
+import Withdraw from "./withdraw";
+import AllData from "./alldata";
+import "./app.css";
 
 function App() {
   return (
-   <HashRouter basename="/bad-bank">
-      <Navbar />
-      <UserContext.Provider value={{users:[{name:'Max',email:'max@mit.edu',password:'secret',balance:1000}]}}>
-
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/create-account' element={<CreateAccount />} />
-          <Route path='/deposit' element={<Deposit />} />
-          <Route path='/withdraw' element={<Withdraw />} />
-          <Route path='/all-data' element={<AllData />} />
-        </Routes>
-      </UserContext.Provider>
-   </HashRouter>
+    <>
+      <HashRouter basename="/bad-bank">
+        <Navbar />
+        <UserContext.Provider
+          value={{
+            users: [
+              {
+                name: "Max",
+                email: "max@mit.edu",
+                password: "secret",
+                balance: 1000,
+              },
+            ],
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-account" element={<CreateAccount />} />
+            <Route path="/deposit" element={<Deposit />} />
+            <Route path="/withdraw" element={<Withdraw />} />
+            <Route path="/all-data" element={<AllData />} />
+          </Routes>
+        </UserContext.Provider>
+      </HashRouter>
+    </>
   );
 }
 
