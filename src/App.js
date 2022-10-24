9f8d320e2adfb636b7747b2676a3e9aed9459a24
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2022-10-23 17:03:59 -0700

    publicfix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,4 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import { UserContext } from "./context";
-import Navbar from "./navbar";
-import Home from "./home";
-import CreateAccount from "./createaccount";
-import Deposit from "./deposit";
-import Withdraw from "./withdraw";
-import AllData from "./alldata";
-import "./app.css";
-=======
 import AllData from "./alldata";
 import "./app.css";
 import { UserContext } from "./context";
@@ -17,16 +7,11 @@ import Deposit from "./deposit";
 import Home from "./home";
 import Navbar from "./navbar";
 import Withdraw from "./withdraw";
->>>>>>> parent of 569b127 (update it)
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <HashRouter basename="/bad-bank">
-=======
-      <HashRouter>
->>>>>>> parent of 569b127 (update it)
         <Navbar />
         <UserContext.Provider
           value={{
